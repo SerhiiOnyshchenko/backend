155cfc2fcebaa45cf70f1e490daa4dbfbd2b637d
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-10-07 11:50:15 +0300

    aws delete

--- src/services/s3service.js
+++ src/services/s3service.js
@@ -6,15 +6,24 @@ const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
 const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
 const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME;
 
+const s3 = new S3({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+});
+
 exports.s3Uploadv2 = async file => {
-  const s3 = new S3({
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-  });
   const params = {
     Bucket: AWS_BUCKET_NAME,
     Key: `upload/${uuidv4()}-${file.originalname}`,
     Body: file.buffer,
   };
   return await s3.upload(params).promise();
+};
+
+exports.s3Deletev2 = async file => {
+  const params = {
+    Bucket: AWS_BUCKET_NAME,
+    Key: `upload/${file.Key}`,
+  };
+  return await s3.deleteObject(params).promise();
 };
